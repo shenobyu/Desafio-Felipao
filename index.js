e4a502697cefcb7faf73b658f336f6cf664dd55f
Marcos <shenobyu@gmail.com>
2023-12-11 14:37:46 -0300

    ta pronto

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-console.log("hello world")
-let duvida = "sera q foi"
+let nome = "shaolin matador de porco"
+let xp = 2200
+let r = "Seu Herói " + nome + " está no nível "
+
+if (xp < 1000){
+    console.log(r + "Ferro")
+}else if(xp >= 1001 & xp <= 2000){
+    console.log(r + "Bronze")
+}else if(xp >= 2001 & xp <= 5000){
+    console.log(r + "Prata")
+}else if(xp >= 6001 & xp <= 7000){
+    console.log(r + "Ouro")
+}else if(xp >= 7001 & xp <= 8000){
+    console.log(r + "Platina")
+}else if(xp >= 8001 & xp <= 9000){
+    console.log(r + "Ascendente")
+}else if(xp >= 9001 & xp <= 10000){
+    console.log(r + "Imortal")
+}else{
+    console.log(r + "Radiante")
+}
+
+
+
+
+
+
